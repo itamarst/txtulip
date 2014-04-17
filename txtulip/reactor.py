f8b64a478b717729cace4d5f648c0ed32b4839f6
Itamar Turner-Trauring <itamar@itamarst.org>
2014-04-17 10:46:04 -0400

    Let's just do PEP8.

--- txtulip/reactor.py
+++ txtulip/reactor.py
@@ -12,7 +12,6 @@ from twisted.python.log import callWithLogger
 from twisted.internet.interfaces import IReactorFDSet
 
 
-
 class _DCHandle(object):
     """
     Wrapper for asyncio.Handle to be used by DelayedCall.
@@ -20,10 +19,8 @@ class _DCHandle(object):
     def __init__(self, handle):
         self.handle = handle
 
-
     def cancel(self):
         self.handle.cancel()
-
 
 
 @implementer(IReactorFDSet)
@@ -40,8 +37,7 @@ class AsyncioSelectorReactor(PosixReactorBase):
         self._delayedCalls = set()
         PosixReactorBase.__init__(self)
 
-
-    def _doReadOrWrite(self, selectable, read):
+    def _read_or_write(self, selectable, read):
         method = selectable.doRead if read else selectable.doWrite
         try:
             why = method()
@@ -50,20 +46,17 @@ class AsyncioSelectorReactor(PosixReactorBase):
         if why:
             self._disconnectSelectable(selectable, why, read)
 
-
     def addReader(self, reader):
         self._readers.add(reader)
         fd = reader.fileno()
         self._asyncioEventloop.add_reader(fd, callWithLogger, reader,
-                                          self._doReadOrWrite, reader, True)
-
+                                          self._read_or_write, reader, True)
 
     def addWriter(self, writer):
         self._writers.add(writer)
         fd = writer.fileno()
         self._asyncioEventloop.add_writer(fd, callWithLogger, writer,
-                                          self._doReadOrWrite, writer, False)
-
+                                          self._read_or_write, writer, False)
 
     def removeReader(self, reader):
         try:
@@ -75,7 +68,6 @@ class AsyncioSelectorReactor(PosixReactorBase):
             return
         self._asyncioEventloop.remove_reader(fd)
 
-
     def removeWriter(self, writer):
         try:
             self._writers.remove(writer)
@@ -86,28 +78,22 @@ class AsyncioSelectorReactor(PosixReactorBase):
             return
         self._asyncioEventloop.remove_writer(fd)
 
-
     def removeAll(self):
         return self._removeAll(self._readers, self._writers)
-
 
     def getReaders(self):
         return list(self._readers)
 
-
     def getWriters(self):
         return list(self._writers)
 
-
     def getDelayedCalls(self):
         return list(self._delayedCalls)
-
 
     def iterate(self, timeout):
         self._asyncioEventloop.call_later(timeout + 0.01,
                                           self._asyncioEventloop.stop)
         self._asyncioEventloop.run_forever()
-
 
     def run(self, installSignalHandlers=True):
         self.startRunning(installSignalHandlers=installSignalHandlers)
@@ -115,20 +101,16 @@ class AsyncioSelectorReactor(PosixReactorBase):
         if self._justStopped:
             self._justStopped = False
 
-
     def stop(self):
         PosixReactorBase.stop(self)
         self.callLater(0, self.fireSystemEvent, "shutdown")
-
 
     def crash(self):
         PosixReactorBase.crash(self)
         self._asyncioEventloop.stop()
 
-
     def seconds(self):
         return self._asyncioEventloop.time()
-
 
     def callLater(self, seconds, f, *args, **kwargs):
         def run():
@@ -148,12 +130,10 @@ class AsyncioSelectorReactor(PosixReactorBase):
         self._delayedCalls.add(dc)
         return dc
 
-
     def callWhenRunning(self, f, *args, **kwargs):
         g = lambda: f(*args, **kwargs)
         self._asyncioEventloop.call_soon_threadsafe(g)
     callFromThread = callWhenRunning
-
 
 
 # Install some testing infrastructure; please don't look at this:
@@ -166,7 +146,6 @@ def _installTestInfrastructure():
     from twisted.internet.test.reactormixins import ReactorBuilder
     ReactorBuilder._reactors.append("txtulip.reactor._reactorForTesting")
 _installTestInfrastructure()
-
 
 
 def install(eventloop=None):
